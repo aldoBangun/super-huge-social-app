4b5fe5c6a05a7c18fb882ddd0b1c386433edd7f6
aldoBangun <punk.aldo68@gmail.com>
2021-07-05 19:47:20 +0800

    initial app setup

---
+++ src/store/index.js
@@ -0,0 +1,23 @@
+import { createStore } from 'vuex';
+
+
+const store = createStore({
+   modules: {},
+   state() {
+      return {
+
+      }
+   },
+   getters: {
+
+   },
+   mutations: {
+
+   },
+   actions: {
+
+   }
+});
+
+
+export default store;
