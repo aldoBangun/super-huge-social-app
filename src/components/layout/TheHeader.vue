67ff4db525c8f3da33ccf9c4f4328ccd5375de80
aldoBangun <punk.aldo68@gmail.com>
2021-07-05 20:02:48 +0800

    create TheHeader.vue components and it's template to components\layout

---
+++ src/components/layout/TheHeader.vue
@@ -0,0 +1,26 @@
+<template>
+   <header>
+      <nav>
+         <div class="nav-brand">
+            <router-link to="/">Tweetbookram</router-link>
+         </div>
+         <ul class="nav-menu">
+            <li class="nav-item">
+               <router-link to="/">Home</router-link>
+            </li>
+            <li class="nav-item">
+               <router-link to="/profile">Profile</router-link>
+            </li>
+         </ul>
+      </nav>
+   </header>
+</template>
+
+<script>
+
+
+</script>
+
+<style scoped>
+
+</style>
