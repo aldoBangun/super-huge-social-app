7a03db43050c9fbcf23aa44882ca6f47cb0963bd
aldoBangun <punk.aldo68@gmail.com>
2021-07-07 14:48:00 +0800

    Add tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
    <TheHeader />
+   <div class="w-1/2 mx-auto">
+      <router-view></router-view>
+   </div>
 </template>
 
 
@@ -14,7 +17,12 @@ export default {
 
 </script>
 
-
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap');
+
+body {
+   background-color: #f2f2f2;
+   font-family: 'Poppins', sans-serif;
+}
 
 </style>
