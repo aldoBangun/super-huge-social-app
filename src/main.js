dfcc1c15c74c2d59716307efe7b1f121c05ae19c
aldoBangun <punk.aldo68@gmail.com>
2021-07-07 11:28:35 +0800

    Import BaseButton and BaseCard as a global components

--- src/main.js
+++ src/main.js
@@ -4,9 +4,16 @@ import App from './App.vue';
 import router from './router/router.js';
 import store from './store/index.js';
 
+// Global Components
+import BaseButton from './components/UI/BaseButton';
+import BaseCard from './components/UI/BaseCard';
+
 const app = createApp(App);
 
 app.use(router);
 app.use(store);
+
+app.component('base-button', BaseButton);
+app.component('base-card', BaseCard);
 
 app.mount('#app');
