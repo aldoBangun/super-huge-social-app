c9392e0600debde69f9e4471ca14e6f5615d6576
aldoBangun <punk.aldo68@gmail.com>
2021-07-07 11:17:59 +0800

    Add BaseButton to UI Components

---
+++ src/components/UI/BaseButton.vue
@@ -0,0 +1,46 @@
+<template>
+   <button class="btn">
+      <slot></slot>
+   </button>
+</template>
+
+
+<style scoped>
+
+.btn {
+   cursor: pointer;
+   border: none;
+   display: inline-flex;
+   align-items: center;
+   justify-content: center;
+   height: 2rem;
+   padding-left: 1.5rem;
+   padding-right: 1.5rem;
+   background-color: var(--clr-blue);
+   border-radius: 9999rem;
+}
+
+.btn:hover {
+   background-color: var(--clr-blue-2);
+}
+
+.btn-md {
+   height: 2.5rem;
+}
+
+.btn-lg {
+   height: 3rem;
+   min-width: 10rem;
+}
+
+.btn-flat {
+   color: var(--clr-gray);
+   background-color: transparent;
+}
+
+.btn-flat:hover {
+   filter: brightness(.8);   
+}
+
+
+</style>
