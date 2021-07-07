12b1d198c935708fea8f487ae8a99e456e4a1164
aldoBangun <punk.aldo68@gmail.com>
2021-07-07 11:09:36 +0800

    Added BaseCard to ./components/UI/BaseCard.vue

---
+++ src/components/UI/BaseCard.vue
@@ -0,0 +1,30 @@
+<template>
+   <div class="card">
+      <slot></slot>
+   </div>
+</template>
+
+<style scoped>
+
+.card {
+   background-color: #fff;
+   padding: 1.5rem;
+   border-radius: 1rem;
+}
+
+.round-sm {
+   border-radius: .5rem;
+}
+
+.round-lg {
+   border-radius: 1.5rem;
+}
+
+.round-xl {
+   border-radius: 2rem;
+}
+
+.round-full {
+   border-radius: 9999rem;
+}
+</style>
