f0de06692ae45e130a4cb0d834c17033e7d3de15
aldoBangun <punk.aldo68@gmail.com>
2021-07-09 12:57:37 +0800

    add search bar to header

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -1,13 +1,24 @@
 <template>
-   <header>
+   <header class="bg-white mb-8">
       <nav class="vw-80 mx-auto h-20 flex-center justify-between">
          <div>
-            <router-link to="/" class="text-blue-500 text-2xl font-bold"
-               >Tweetbookram</router-link
-            >
+            <router-link to="/" class="text-blue-500 text-2xl font-bold">
+               Tweetbookram
+            </router-link>
          </div>
+
+         <form @submit.prevent="searchUser" class="w-80 relative">
+            <input type="text" v-model="inputedKeyword" class="w-full rounded-full bg-gray-100 pl-10 pr-4 py-1">
+            <base-button class="absolute top-1/2 left-1 transform -translate-y-1/2 text-gray-500">
+               <b-icon-search></b-icon-search>
+            </base-button>
+         </form>
+
          <div class="text-lg">
-            <ul class="nav-page flex-center gap-4 text-gray-400" v-if="isAuthenticatedUser">
+            <ul
+               class="nav-page flex-center gap-4 text-gray-400"
+               v-if="isAuthenticatedUser"
+            >
                <li>
                   <router-link to="/">Home</router-link>
                </li>
@@ -43,18 +54,27 @@
 </template>
 
 <script>
+import { BIconSearch } from 'bootstrap-icons-vue'
+
 export default {
+   components: {
+      BIconSearch
+   },
    data() {
       return {
-         isAuthenticatedUser: true
+         isAuthenticatedUser: true,
+         inputedKeyword: ''
+      };
+   },
+   methods: {
+      searchUser() {
+         console.log('user')
       }
    }
 };
 </script>
 
 <style scoped>
-
-
 .flex-center {
    @apply flex items-center;
 }
